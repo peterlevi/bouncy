3a759c10ec0bba5c9d9a0437baaa55e0f067435f
Peter Levi <peterlevi@peterlevi.com>
2019-04-04 02:09:03 +0300

    Initial stuff: Ball, Platform, working mobx setup

--- src/App.tsx
+++ src/App.tsx
@@ -1,28 +1,86 @@
-import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from "react";
+import { action, observable } from "mobx";
+import { observer } from "mobx-react";
+import "./App.css";
 
-class App extends Component {
-  render() {
-    return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.tsx</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
-      </div>
-    );
+const HEIGHT = 600;
+
+interface IBall {
+  x: number;
+  y: number;
+  radius: number;
+}
+
+interface IPlatform {
+  x: number;
+  y: number;
+  width: number;
+}
+
+class Model {
+  @observable ball: IBall = { x: 50, y: 300, radius: 10 };
+  @observable platforms: IPlatform[] = [{ x: 100, y: 100, width: 200 }];
+
+  @action update() {
+    this.ball.x += 1;
+  }
+
+  start() {
+    setInterval(this.update.bind(this), 20);
   }
 }
 
-export default App;
+const model = new Model();
+(window as any).model = model;
+
+
+const Ball = observer(({
+  ball: { x, y, radius },
+  color = "red"
+}: {
+  ball: IBall;
+  color?: any;
+}) => (
+  <div
+    className="Ball"
+    style={{
+      left: x - radius,
+      top: HEIGHT - (y - radius),
+      width: 2 * radius,
+      height: 2 * radius,
+      backgroundColor: color
+    }}
+  />
+));
+
+const Platform = observer(({ x, y, width }: IPlatform) => (
+  <div
+    className="Platform"
+    style={{
+      left: x,
+      top: HEIGHT - y,
+      width
+    }}
+  />
+));
+
+@observer
+class Game extends Component<{ model: Model }> {
+  render() {
+    const { ball, platforms } = this.props.model;
+    return (
+      <div className="root">
+        <Ball ball={ball} />
+        {platforms.map(platform => (
+          <Platform {...platform} key={[platform.x, platform.y].toString()} />
+        ))}
+      </div>
+    );
+  }
+
+  componentDidMount(): void {
+    this.props.model.start();
+  }
+}
+
+export const App = () => <Game model={model} />;
